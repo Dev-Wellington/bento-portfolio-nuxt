526abccc608a54566652e99b6219ada38d2af985
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-12-28 15:49:56 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/">Projects</NuxtLink></li>
+          <li><NuxtLink to="/">Contact</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <footer>
+      <p>&copy; 2024 - {{ new Date().getFullYear() }} Built by Wellington</p>
+      <button>
+        <svg>dark</svg>
+        <svg>light</svg>
+        <span>Toggle theme</span>
+      </button>
+    </footer>
+  </div>
+</template>
---
+++ pages/contact.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container-contact">
+    <div class="contact__wrapper">
+      <div class="contact-info">
+        <div>
+          <h3>meuemail@meu.com</h3>
+        </div>
+        <div>
+          <img src="" alt="logo linkedin ou github" />
+        </div>
+      </div>
+      <div class="contact-calendar">
+        <div>
+          <img src="" alt="Calendy" />
+          <p></p>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>
+          <svg></svg>
+          <svg></svg>
+          <span>sr only</span>
+        </button>
+      </div>
+    </div>
+    <div class="contact__form">
+        <form>
+            <h2>Me contate ou Me contate atraves do form </h2>
+            <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" />
+            </div>
+            <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" />
+            </div>
+            <div>
+            <label for="message">Menssage</label>
+            <textarea id="message"></textarea>
+            </div>
+            <button>Send a message</button>
+        </form>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
