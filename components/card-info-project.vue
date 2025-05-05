6cfacf3bf03aed4226030fd385cfd4d24560ac93
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-05 20:53:04 -0300

    feat: add card-info-project component

---
+++ components/card-info-project.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <span>
+        <h2>Pomodoro</h2>
+        <icon></icon>
+      </span>
+      <p>
+        Aplicação desenvolvida com o objetivo de ajudar usuários a gerenciar seu
+        tempo de forma eficiente, utilizando a técnica Pomodoro.
+      </p>
+    </div>
+    <div>
+      <!-- Habilidades -->
+      <span>aaaa</span>
+      <span>aaa</span>
+      <span>aaa</span>
+    </div>
+    <div>
+      <click></click>
+      <github></github>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
