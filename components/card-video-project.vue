299b5b3298c2db431e55668b3e66d08e3b056802
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-05 19:16:48 -0300

    feat: add video component

---
+++ components/card-video-project.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <video
+            v-for="video in videos"
+            :key="video.id"
+            :src="video.src"
+            type="video/mp4"
+            @mouseenter="playV"
+            @mouseleave="resetV"
+            muted
+        >
+            Your browser does not support the video tag.
+        </video>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const videos = ref([
+    { id: 1, src: 'link' },
+    { id: 2, src: 'link' },
+    { id: 3, src: 'link' },
+]);
+
+
+const playV = (event: Event) => {
+    const video = event.target as HTMLVideoElement;
+    video.play();
+};
+
+
+const resetV = (event: Event) => {
+    const video = event.target as HTMLVideoElement;
+    video.pause();
+    video.currentTime = 0; 
+};
+</script>
+
+<style scoped>
+
+</style>
