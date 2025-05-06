281e97bebf0b8f3e2dea3ff9b1d773f2b83de016
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-06 18:32:12 -0300

    feat: add linkedin button

---
+++ components/btn-minimal-linkedin.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <span>
+            <icon-linkedin/>
+            <h3>LinkedIn</h3>
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+div{
+    display: flex;
+    border-radius: 15px;
+    padding: 20px;
+    width:  min(200px,100%);
+    background-color: #171717;
+}
+span{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+    height: 100%;
+    
+    cursor: pointer;
+    
+}
+</style>
