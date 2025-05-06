40fbed244c478c1bc7d17ed534ec1e7db51d0715
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-05 21:10:05 -0300

    feat: add card-content component

---
+++ components/card-project-content.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <card-info-project/>
+        <card-video-project/>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+div{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    width: 100%;
+    height: 100%;
+    
+}
+</style>
