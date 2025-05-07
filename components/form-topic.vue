9fc38c22bccab685c120312fc8a81d6b3d4a637b
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-07 15:38:28 -0300

    feat: add form-topic component with validation and placeholders

---
+++ components/form-topic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label for="topic"></label>
+    <input
+      type="topic"
+      id="topic"
+      name="topic"
+      :placeholder="topicPlaceholder"
+      required
+      minlength="10"
+      maxlength="35"
+      :title="topicTitle"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const topicPlaceholder = "Enter your topic";
+const topicTitle = "Please enter a valid topic";
+</script>
+
+<style scoped></style>
