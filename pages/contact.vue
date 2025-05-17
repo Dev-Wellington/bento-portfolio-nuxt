cd1351d8e38d6028cb3b5a07b1dc3211b1987770
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-17 20:47:44 -0300

    refactor: restructure contact.vue template for improved readability

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-
+    <div class="socials">
+      <div class="email-github">
+        <btn-email />
+        <btn-github-profile />
+      </div>
+      <btn-large-linkedin />
+    </div>
+    <div class="form">
+      <h2>Entre em contato pelo formulário abaixo</h2>
+      <component></component>
+    </div>
   </div>
 </template>
 
