b42cc301bc8512b4c5deeb1947960d821f4f9aeb
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-17 20:49:29 -0300

    feat: add btn-large-linkedin component with LinkedIn profile link

---
+++ components/btn-large-linkedin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <icon-linkedin />
+      <h3>LinkedIn</h3>
+      <p>Interessado no meu perfil?</p>
+      <p>No LinkedIn, você encontra mais sobre minha trajetória e projetos.</p>
+    </div>
+    <a
+      href="https://www.linkedin.com/in/wellington-ds-silva/"
+      target="_blank"
+      rel="noopener"
+      class="btn-linkedin"
+      >Conecte-se</a
+    >
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
