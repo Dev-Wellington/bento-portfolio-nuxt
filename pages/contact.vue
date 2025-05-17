e5206b8f1a4494f200fe7bf8e1c88aeee206f7de
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-17 17:07:04 -0300

    refactor: remove commented-out code from contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,60 +1,13 @@
 <template>
-  <!-- <div>
-    <section>
-      <div>
-        <div>
-          <icon-email/>
-          <btn-email/>
-        </div>
-        <div>github</div>
-      </div>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <h2>LinkedIn</h2>
-          <p>Interessado no meu perfil?</p>
-          <p>
-            No LinkedIn, você encontra mais sobre minha trajetória e projetos.
-          </p>
-        </div>
-        <button>Conecte-se</button>
-      </div>
-    </section>
-    <section>
-      <h3>Entre em contato pelo formulário abaixo</h3>
-    </section>
-    <section>
-      <div>
-        <label for="assunto">Assunto:</label>
-        <input
-          type="text"
-          id="assunto"
-          name="assunto"
-          placeholder="Digite o assunto da sua mensagem"
-        />
-      </div>
-      <div>
-        <label for="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Digite seu endereço de email"
-        />
-      </div>
-      <div>
-        <label for="message">Mensagem:</label>
-        <textarea
-          id="message"
-          name="message"
-          placeholder="Escreva sua mensagem aqui"
-        ></textarea>
-      </div>
-      <button type="submit">Enviar Mensagem</button>
-    </section>
-  </div> -->
+  <div>
+
+  </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
 
-<style scoped></style>
+</script>
+
+<style scoped>
+
+</style>
