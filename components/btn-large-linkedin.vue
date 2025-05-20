99d596fe783c89304aa2589d84361d6ce5697670
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-20 10:54:32 -0300

    refactor: update btn-large-linkedin component structure and styles for consistency

--- components/btn-large-linkedin.vue
+++ components/btn-large-linkedin.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="back-container">
-    <div class="back">
-      <icon-linkedin class="linkedin-icon" />
+  <div class="linkedin-card-container">
+    <div class="linkedin-card-content">
+      <icon-linkedin class="linkedin-card-icon" />
       <h3>LinkedIn</h3>
-      <div class="text">
+      <div class="linkedin-card-text">
         <p>Interessado no meu perfil?
         No LinkedIn, você encontra mais sobre minha trajetória e projetos.</p>
-
       </div>
     </div>
     <a
       href="https://www.linkedin.com/in/wellington-ds-silva/"
       target="_blank"
       rel="noopener"
-      class="btnL"
+      class="linkedin-card-btn"
       >Conecte-se</a
     >
   </div>
@@ -22,18 +21,18 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-.back-container {
+.linkedin-card-container {
   display: flex;
   flex-direction: column;
-  padding: 30px;
+  padding: 20px 30px;
   width: min(400px, 100%);
   height: 300px;
   background-color: #f5f5f5;
   border-radius: 20px;
   color: #525252;
- 
+  gap: 20px;
 }
-.back{
+.linkedin-card-content {
   display: flex;
   flex-direction: column;
   gap: 12px;
@@ -43,12 +42,16 @@ h3 {
 }
 p {
   font-size: 1.25rem;
+  line-height: 1.25;
+  font-weight: 400;
 }
-.linkedin-icon {
+.linkedin-card-icon {
   width: 60px;
   height: 60px;
 }
-.btnL {
+.linkedin-card-text {
+}
+.linkedin-card-btn {
   display: flex;
   justify-content: center;
   align-items: center;
