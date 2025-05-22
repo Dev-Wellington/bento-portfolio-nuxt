3209aeaeff4fb277c7666a465be6799250ae176f
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-22 19:01:31 -0300

    refactor: update button text and enhance styling for improved user interaction

--- components/form-button.vue
+++ components/form-button.vue
@@ -5,10 +5,26 @@
 </template>
 
 <script setup lang="ts">
-const send: string = "Send";
+const send: string = "Enviar Mensagem";
 const sendMessage = () => {
   console.log("Message sent!");
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+div {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+button{
+  width: 100%;
+  height: 55px;
+  background-color: #e5e5e5;
+  color: #1d0e04;
+  border: none;
+  border-radius: 15px;
+}
+</style>
