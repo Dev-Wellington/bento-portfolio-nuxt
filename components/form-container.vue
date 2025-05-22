9f227a9c32fe6068ed49096445528241f2abba77
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-05-22 18:43:24 -0300

    refactor: improve form structure and styling for better organization

--- components/form-container.vue
+++ components/form-container.vue
@@ -1,22 +1,37 @@
 <template>
-    <form @submit.prevent="handleSubmit">
+  <div class="form-container">
+    <div class="field">
+      <form @submit.prevent="handleSubmit">
+        <form-info />
         <form-topic />
         <form-email />
         <form-message />
         <form-button />
-    </form>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const handleSubmit = () => {
-    console.log("Form submitted!");
+  console.log("Form submitted!");
 };
 </script>
 
 <style scoped>
+.form-container {
+  width: 100%;
+
+  
+}
+.field {
+  display: flex;
+  flex-direction: column;
+  width: min(915px, 100%);
+}
 form {
-    display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
 }
 </style>
