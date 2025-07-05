5729e46c95c5e44e7c5b5eab184e3ce9163ed198
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-05 07:01:52 -0300

    refactor

---
+++ components/contact/btn-email.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="btn-email__background">
+        <div class="btn-email__mail">
+            <icon-mail class="btn-email__icon"/>
+            <a class="btn-email__text" href="mailto:wellingtonsantosprogrammer@gmail.com">
+                wellingtonsantosprogrammer@gmail.com
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.btn-email__background{
+    width: 400px;
+    height: 125px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+    background-color: #f5f5f5;
+    border-radius: 20px;
+}
+.btn-email__mail{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 360px;
+    height: 80px;
+    border-radius: 25px;
+    gap:5px;
+    background-color: #e5e5e5;
+    color: #525252;
+    
+}
+
+.btn-email__icon{
+    width: 40px;
+    height: 40px;
+}
+.btn-email__text{
+    text-decoration: none;
+    font-size: 1rem;
+    color: #525252;
+}
+.btn-email__text:hover{
+    color: #262626;
+}
+</style>
---
+++ components/contact/btn-large-linkedin.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="linkedin-card-container">
+    <div class="linkedin-card-content">
+      <icon-linkedin class="linkedin-card-icon" />
+      <h3>LinkedIn</h3>
+      <div class="linkedin-card-text">
+        <p>Interessado no meu perfil?
+        No LinkedIn, você encontra mais sobre minha trajetória e projetos.</p>
+      </div>
+    </div>
+    <a
+      href="https://www.linkedin.com/in/wellington-ds-silva/"
+      target="_blank"
+      rel="noopener"
+      class="linkedin-card-btn"
+      >Conecte-se</a
+    >
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.linkedin-card-container {
+  display: flex;
+  flex-direction: column;
+  padding: 20px 30px;
+  width: min(400px, 100%);
+  height: 300px;
+  background-color: #f5f5f5;
+  border-radius: 20px;
+  color: #525252;
+  gap: 20px;
+}
+.linkedin-card-content {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+h3 {
+  font-size: 1.25rem;
+}
+p {
+  font-size: 1.25rem;
+  line-height: 1.25;
+  font-weight: 400;
+}
+.linkedin-card-icon {
+  width: 60px;
+  height: 60px;
+}
+.linkedin-card-text {
+}
+.linkedin-card-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: min(120px, 100%);
+  height: 40px;
+  text-decoration: none;
+  border-radius: 15px;
+  background-color: #e5e5e5;
+  color: #525252;
+}
+</style>
