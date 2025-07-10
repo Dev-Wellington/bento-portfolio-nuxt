39b6e94d4d0b3386cf6d52ce52043efb2779d8b4
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-10 20:28:51 -0300

    feat: add profile photo component

---
+++ components/home/profile-photo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <img :src="profile" alt="">
+    </div>
+</template>
+
+<script setup lang="ts">
+const profile: string = new URL('@/assets/images/profile.jpg', import.meta.url).href;
+</script>
+
+<style scoped>
+
+</style>
