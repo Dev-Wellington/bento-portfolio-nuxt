81a66ac8794b28dc3ef7f99ef12d36a929958506
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-10 20:25:38 -0300

    feat: add btn-location component

---
+++ components/home/btn-location.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- <img src="" alt=""> -->
+         <p>Rio de Janeiro</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
