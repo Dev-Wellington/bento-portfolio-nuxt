07e39700a6a878084e830b4839a40b8c05759a2a
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-10 20:29:36 -0300

    feat: add right-side component

---
+++ components/home/right-side.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <home-about />
+        <home-card-resume />
+        <home-skills-carousel />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
