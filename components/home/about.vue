263350e137ad276351a159084fa69ef25bc1aea3
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-10 20:23:21 -0300

    refactor: bind title and description

--- components/home/about.vue
+++ components/home/about.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        <h1>SOBRE MIM</h1>
-        <p>Sou um carioca de 21 anos que sempre foi apaixonado por artes e desenho, e acabou encontrando na tecnologia uma nova forma de se expressar. Hoje, estudo Engenharia de Software e venho aprimorando minhas soft e hard skills com foco em desenvolvimento e design.</p>
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const title = ref('SOBRE MIM')
+const description = ref('Sou um carioca de 21 anos que sempre foi apaixonado por artes e desenho, e acabou encontrando na tecnologia uma nova forma de se expressar. Hoje, estudo Engenharia de Software e venho aprimorando minhas soft e hard skills com foco em desenvolvimento e design.')
 </script>
 
 <style scoped>
