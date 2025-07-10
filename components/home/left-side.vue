4ea0eec3d7568dea43b70e2ab0539d8c14f2bc13
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-10 20:27:54 -0300

    feat: add left-side

---
+++ components/home/left-side.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <home-profile-photo />
+    <background-photo />
+
+    <home-profile-header />
+
+    <home-btn-send-me />
+    <home-btn-location />
+
+    <home-certificates-carousel />
+
+    <btn-minimal-linkedin />
+    <btn-github-profile />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
