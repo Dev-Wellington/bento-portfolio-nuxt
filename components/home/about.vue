49152fb509f67003766e48dff613f5e0b782e988
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-13 08:05:17 -0300

    style: initializing style

--- components/home/about.vue
+++ components/home/about.vue
@@ -1,17 +1,29 @@
 <template>
-    <div>
-        <h1>{{ title }}</h1>
-        <p>{{ description }}</p>
-    </div>
+  <div class="home-about">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const title = ref('SOBRE MIM')
-const description = ref('Sou um carioca de 21 anos que sempre foi apaixonado por artes e desenho, e acabou encontrando na tecnologia uma nova forma de se expressar. Hoje, estudo Engenharia de Software e venho aprimorando minhas soft e hard skills com foco em desenvolvimento e design.')
+const title = ref("SOBRE MIM");
+const description = ref(
+  "Sou um carioca de 21 anos que sempre foi apaixonado por artes e desenho, e acabou encontrando na tecnologia uma nova forma de se expressar. Hoje, estudo Engenharia de Software e venho aprimorando minhas soft e hard skills com foco em desenvolvimento e design."
+);
 </script>
 
 <style scoped>
+.home-about {
+  display: flex;
+  flex-direction: column;
 
+  justify-content: center;
+  
+
+  & p {
+    text-align: justify
+  }
+}
 </style>
