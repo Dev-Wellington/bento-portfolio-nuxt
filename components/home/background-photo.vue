6ed5e17938a48d2ac01f7b6a83537aa45be058c0
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-13 10:57:48 -0300

    feat: add background photo

---
+++ components/home/background-photo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <img :src="background" alt="background photo" />
+    </div>
+</template>
+
+<script setup lang="ts">
+const background: string = '/img/EXbackground.png';
+</script>
+
+<style scoped>
+</style>
