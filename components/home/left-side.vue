3b681d915bb7bbee8d9ef342c4ad9658a6b92a43
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-07-13 11:01:55 -0300

    style: restructure layout

--- components/home/left-side.vue
+++ components/home/left-side.vue
@@ -1,24 +1,51 @@
 <template>
   <div class="home-left-side">
-    <home-profile-photo />
-    <background-photo />
+    <div class="profile-photos">
+      <home-profile-photo />
+      <home-background-photo />
+    </div>
 
     <home-profile-header />
 
-    <home-btn-send-me />
-    <home-btn-location />
+    <div class="profile-actions">
+      <home-btn-send-me />
+      <home-btn-location />
+    </div>
 
     <home-certificates-carousel />
 
-    <btn-minimal-linkedin />
-    <btn-github-profile />
+    <div class="profile-social-links">
+      <btn-minimal-linkedin />
+      <btn-github-profile />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
-.home-left-side{
-   max-width: 450px;
+.home-left-side {
+  max-width: 450px;
+}
+.profile-photos {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+}
+.profile-actions {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+}
+.profile-social-links {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
 }
 </style>
