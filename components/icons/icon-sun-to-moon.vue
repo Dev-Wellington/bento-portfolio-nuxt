6b8310758026cc13bdfd8f443a4f8660520d4b28
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-10-30 17:58:17 -0300

    style: update icon components

--- components/icons/icon-sun-to-moon.vue
+++ components/icons/icon-sun-to-moon.vue
@@ -1,13 +1,14 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="2rem"
+    height="2rem"
     viewBox="0 0 24 24"
   >
+    <!-- Icon from Material Line Icons by Vjacheslav Trushkin - https://github.com/cyberalien/line-md/blob/master/license.txt -->
     <g
       fill="none"
-      stroke="currentColor"
+      stroke="#a3a3a3"
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
@@ -23,14 +24,14 @@
           begin="0.6s"
           dur="0.2s"
           values="M12 19v1M19 12h1M12 5v-1M5 12h-1;M12 21v1M21 12h1M12 3v-1M3 12h-1"
-        ></animate>
+        />
         <animate
           fill="freeze"
           attributeName="stroke-dashoffset"
           begin="0.6s"
           dur="0.2s"
           values="2;0"
-        ></animate>
+        />
       </path>
       <path
         stroke-dasharray="2"
@@ -43,14 +44,14 @@
           begin="0.8s"
           dur="0.2s"
           values="M17 17l0.5 0.5M17 7l0.5 -0.5M7 7l-0.5 -0.5M7 17l-0.5 0.5;M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5"
-        ></animate>
+        />
         <animate
           fill="freeze"
           attributeName="stroke-dashoffset"
           begin="0.8s"
           dur="0.2s"
           values="2;0"
-        ></animate>
+        />
       </path>
       <animateTransform
         attributeName="transform"
@@ -58,77 +59,31 @@
         repeatCount="indefinite"
         type="rotate"
         values="0 12 12;360 12 12"
-      ></animateTransform>
+      />
     </g>
-    <mask id="lineMdMoonAltToSunnyOutlineLoopTransition0">
-      <circle cx="12" cy="12" r="12" fill="#fff"></circle>
+    <mask id="SVGpCD1890p">
+      <circle cx="12" cy="12" r="12" fill="#fff" />
       <circle cx="12" cy="12" r="8">
-        <animate
-          fill="freeze"
-          attributeName="r"
-          dur="0.4s"
-          values="8;4"
-        ></animate>
+        <animate fill="freeze" attributeName="r" dur="0.4s" values="8;4" />
       </circle>
       <circle cx="18" cy="6" r="12" fill="#fff">
-        <animate
-          fill="freeze"
-          attributeName="cx"
-          dur="0.4s"
-          values="18;22"
-        ></animate>
-        <animate
-          fill="freeze"
-          attributeName="cy"
-          dur="0.4s"
-          values="6;2"
-        ></animate>
-        <animate
-          fill="freeze"
-          attributeName="r"
-          dur="0.4s"
-          values="12;3"
-        ></animate>
+        <animate fill="freeze" attributeName="cx" dur="0.4s" values="18;22" />
+        <animate fill="freeze" attributeName="cy" dur="0.4s" values="6;2" />
+        <animate fill="freeze" attributeName="r" dur="0.4s" values="12;3" />
       </circle>
       <circle cx="18" cy="6" r="10">
-        <animate
-          fill="freeze"
-          attributeName="cx"
-          dur="0.4s"
-          values="18;22"
-        ></animate>
-        <animate
-          fill="freeze"
-          attributeName="cy"
-          dur="0.4s"
-          values="6;2"
-        ></animate>
-        <animate
-          fill="freeze"
-          attributeName="r"
-          dur="0.4s"
-          values="10;1"
-        ></animate>
+        <animate fill="freeze" attributeName="cx" dur="0.4s" values="18;22" />
+        <animate fill="freeze" attributeName="cy" dur="0.4s" values="6;2" />
+        <animate fill="freeze" attributeName="r" dur="0.4s" values="10;1" />
       </circle>
     </mask>
-    <circle
-      cx="12"
-      cy="12"
-      r="10"
-      mask="url(#lineMdMoonAltToSunnyOutlineLoopTransition0)"
-      fill="currentColor"
-    >
-      <animate
-        fill="freeze"
-        attributeName="r"
-        dur="0.4s"
-        values="10;6"
-      ></animate>
+    <circle cx="12" cy="12" r="10" mask="url(#SVGpCD1890p)" fill="#a3a3a3">
+      <animate fill="freeze" attributeName="r" dur="0.4s" values="10;6" />
     </circle>
   </svg>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: "LineMdMoonAltToSunnyOutlineLoopTransition",
 };
