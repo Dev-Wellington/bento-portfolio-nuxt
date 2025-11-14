42c53bffb27f8bbed83d9720723d8a1c9585e183
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2025-11-14 07:23:17 -0300

    style: remove unused video component and simplify template to display image

--- components/projects/card-video-project.vue
+++ components/projects/card-video-project.vue
@@ -1,55 +1,26 @@
 <template>
-    <div>
-        <!-- <video
-            v-for="video in videos"
-            :key="video.id"
-            :src="video.src"
-            type="video/mp4"
-            @mouseenter="playV"
-            @mouseleave="resetV"
-            muted
-        >
-            Your browser does not support the video tag.
-        </video> -->
-    </div>
+  <div>
+    <img :src="image" alt="Image Project" />
+  </div>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue';
-
-// const videos = ref([
-//     { id: 1, src: 'link' },
-//     { id: 2, src: 'link' },
-//     { id: 3, src: 'link' },
-// ]);
-
-
-// const playV = (event: Event) => {
-//     const video = event.target as HTMLVideoElement;
-//     video.play();
-// };
-
-
-// const resetV = (event: Event) => {
-//     const video = event.target as HTMLVideoElement;
-//     video.pause();
-//     video.currentTime = 0; 
-// };
+const image: string = "/img/Pomodoro.png";
 </script>
 
 <style scoped>
-div{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width:min(500px,100%);
-    height: min(360px,100%);
-    background-color: red;
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: min(500px, 100%);
+  height: min(360px, 100%);
 }
- /*video{
-    width: 100%;
-    height: 100%;
-    border-radius: 20px;
-    
-}*/
+img {
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+  object-fit: cover;
+}
+
 </style>
